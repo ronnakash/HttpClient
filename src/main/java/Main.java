8b24995219474c21ebb00a32fac065dd2540bee5
Ron <ron.nakash@gmtonline.co.il>
2023-07-30 22:51:24 +0300

    omcr

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,18 @@
+import client.ObjectMapperContextResolver;
+import org.glassfish.jersey.client.JerseyClientBuilder;
+
+import javax.ws.rs.client.Client;
+import java.lang.module.Configuration;
 
 public class Main {
 
 	public static void main(String[] args){
 		System.out.println("hello!");
+		Client clientBuilder = new
+				JerseyClientBuilder()
+				.register(new ObjectMapperContextResolver())
+				.build();
+
 	}
 
 }
