ea92db13df4b0391db981be2d53d9ef9ca827d19
Ron <ron.nakash@gmtonline.co.il>
2023-07-31 09:45:08 +0300

    success

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,10 @@
-import client.ObjectMapperContextResolver;
+import providers.JsonProvider;
+import providers.ObjectMapperContextResolver;
 import org.glassfish.jersey.client.JerseyClientBuilder;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
-import java.lang.module.Configuration;
 import java.util.List;
 
 public class Main {
@@ -13,18 +13,33 @@ public class Main {
 		try {
 			Client client = new JerseyClientBuilder()
 //					.register(new ObjectMapperContextResolver())
+					.register(new JsonProvider<>())
 					.build();
 
-			List<List<Integer>> result = client
+			List<String> result = client
 					.target("http://localhost:9090/test/request")
 					.request(MediaType.APPLICATION_JSON_TYPE)
-					.get(new GenericType<>(){});
+					.get(new GenericType<>() {});
 
 			System.out.println(result);
 
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
+	}
+
+
+	private <T> T test(Class<T> clazz) {
+		Client client = new JerseyClientBuilder()
+//					.register(new ObjectMapperContextResolver())
+				.register(new JsonProvider<>())
+				.build();
+
+		return client
+				.target("http://localhost:9090/test/request")
+				.request(MediaType.APPLICATION_JSON_TYPE)
+				.get(new GenericType<>() {});
+
 	}
 
 }
--- src/main/java/providers/ObjectMapperContextResolver.java
+++ src/main/java/providers/ObjectMapperContextResolver.java
@@ -1,4 +1,4 @@
-package client;
+package providers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
