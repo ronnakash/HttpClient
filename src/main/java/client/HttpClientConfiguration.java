86756b2e937d04804da86dcec0ca8a13014d5a9c
Ron <ron.nakash@gmtonline.co.il>
2023-08-01 17:37:00 +0300

    config

--- src/main/java/client/HttpClientConfiguration.java
+++ src/main/java/client/HttpClientConfiguration.java
@@ -5,7 +5,15 @@ import javax.ws.rs.core.Configuration;
 //TODO: make this class take our body parsers
 
 public class HttpClientConfiguration {
+
     private Configuration config;
+
+    public HttpClientConfiguration(Configuration config) {
+        this.config = config;
+    }
+
+    public HttpClientConfiguration() {
+    }
 
     protected Configuration getJerseyConfig() {
         return config;
